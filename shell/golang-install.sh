d0846f32ec786fc2e72bffbbff5fe4466dd9930f
Xing Xing <mikespook@gmail.com>
2013-05-11 17:51:16 +0800

    fixed path

--- shell/golang-install.sh
+++ shell/golang-install.sh
@@ -4,7 +4,7 @@
 
 check_base_env
 
-if [[ $EUID -e 0 ]]; then
+if [ $EUID -e 0 ]; then
     apt-get install -y gcc libc6-dev mercurial
     if [ "$?" != "0" ]; then 
     	echo "error, check your network!"
@@ -12,16 +12,20 @@ if [[ $EUID -e 0 ]]; then
     fi
 fi
 
+BIN=$SERVICE_BASE/bin
+OWN=$SERVICE_BASE/golang/own
+PKG=$SERVICE_BASE/golang/3rdpkg
+
 # init the environment
-mkdir -p $SERVICE_BASE/bin
-mkdir -p $SERVICE_BASE/go/own
-mkdir -p $SERVICE_BASE/go/3rdpkg
+mkdir -p $BIN
+mkdir -p $OWN
+mkdir -p $PKG
 # setup env-variables
-export PATH=$PATH:$SERVICE_BASE/bin
-export GOROOT=$SERVICE_BASE/go/golang
-export GOBIN=$SERVICE_BASE/bin
+export PATH=$PATH:$BIN
+export GOROOT=$SERVICE_BASE/golang/go
+export GOBIN=$BIN
 # put $SERVICE_BASE/go/3rdpkg at the first
-export GOPATH=$SERVICE_BASE/go/3rdpkg:$SERVICE_BASE/go/own:$GOROOT
+export GOPATH=$PKG:$OWN:$GOROOT
 export GOTOOLDIR=$GOROOT/pkg/tool
 
 if [ $SERVICE_BASE == $HOME ]; then
@@ -46,12 +50,12 @@ sed -i -e "s/^export GOTOOLDIR.*//g" $config
 echo "" >>$config
 echo "export SERVICE_BASE=$SERVICE_BASE" >> $config
 echo 'export GOBIN=$SERVICE_BASE/bin' >> $config
-echo 'export GOROOT=$SERVICE_BASE/go/golang' >> $config
-echo 'export GOPATH=$SERVICE_BASE/go/3rdpkg:$SERVICE_BASE/go/own:$GOROOT' >> $config
+echo 'export GOROOT=$SERVICE_BASE/golang/go' >> $config
+echo 'export GOPATH=$SERVICE_BASE/golang/3rdpkg:$SERVICE_BASE/golang/own:$GOROOT' >> $config
 echo 'export GOTOOLDIR=$GOROOT/pkg/tool' >> $config
 echo 'export PATH=$PATH:$GOBIN' >> $config
 
-cd $SERVICE_BASE/go
+cd $SERVICE_BASE/golang
 rm -rf golang
 hg clone -u release https://code.google.com/p/go
 if [ "$?" != "0" ]; then 
@@ -68,7 +72,7 @@ if [ "$?" != "0" ]; then
 fi
 
 echo <<EOF 
-Golang was installed at $SERVICE_BASE/go/golang.
+Golang was installed at $SERVICE_BASE/golang/go.
 -----
 done!
 -----
