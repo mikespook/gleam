e9faf94e0d197bef94921d68f18b1e9be749bc00
Xing Xing <mikespook@gmail.com>
2017-04-26 14:51:31 +1200

    fixed cp

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -5,7 +5,7 @@
 cp gleam /usr/local/bin/
 cp init.d_gleam /etc/init.d/gleam
 mkdir -p /usr/local/share/gleam
-cp scripts/* /usr/local/share/gleam/
+cp -r scripts/* /usr/local/share/gleam/
 
 update-rc.d -f gleam remove
 update-rc.d gleam defaults
